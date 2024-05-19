37b46358062d6bca3388d7d0995b5f022f8fb588
YanRodrigues <133587638+Yan-MR@users.noreply.github.com>
2024-05-18 21:14:59 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import ConversorDeMoedas from './App.jsx'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConversorDeMoedas/>
+  </React.StrictMode>,
+)
